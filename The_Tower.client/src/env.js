386ee55c19152cccf88570ca04036d4d11c0c813
Austin <austinjperry22@gmail.com>
2023-09-19 17:25:12 -0600

    env

--- The_Tower.client/src/env.js
+++ The_Tower.client/src/env.js
@@ -1,6 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-lucu1eq62l0de56p.us.auth0.com'
+export const clientId = 'Pm5TQLhJkButJ3ZIrllfYVfdTz1CRJbD'
+export const audience = 'https://codeworksAPI.com'
+
