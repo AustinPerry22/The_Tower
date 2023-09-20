4e2f32802128ef86089d69a51de5d0752e2af691
Austin <austinjperry22@gmail.com>
2023-09-20 15:37:47 -0600

    got events

--- The_Tower.client/src/pages/HomePage.vue
+++ The_Tower.client/src/pages/HomePage.vue
@@ -1,23 +1,59 @@
 <template>
   <section class="row justify-content-center">
-    <img src="https://images.unsplash.com/photo-1532621623813-d37d9cd1d992?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNlYXRzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" alt="picture of seats" id="topImg" class="col-11 elevation-5">
+    <div class="col-11 justify-content-center" id="top-card">
+      <h3>Lorem ipsum dolor sit amet.</h3>
+      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing.</h3>
+      <h3>Lorem ipsum dolor sit.</h3>
+    </div>
+  </section>
+  <section class="row">
+    navbar component
+  </section>
+  <section class="row justify-content-evenly">
+    <div v-for="event in events" :key="event.id" class="col-3 event-card">
+      event
+    </div>
   </section>
 </template>
 
 <script>
+import { computed, onMounted } from 'vue';
+import { AppState } from '../AppState';
+import {eventsService} from '../services/EventsService'
+import Pop from '../utils/Pop';
+
 export default {
   setup() {
-    return {}
+    onMounted(()=>getEvents())
+    async function getEvents(){
+      try {
+        await eventsService.getEvents()
+      } catch (error) {
+        Pop.error(error)
+      }
+    }
+    return {
+      events: computed(()=>AppState.events)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-#topImg{
-  height: 35vh;
-  width: 100%;
-  object-fit: cover;
-  object-position: center;
+#top-card{
+  background-image: url(https://images.unsplash.com/photo-1532621623813-d37d9cd1d992?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNlYXRzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60);
+  background-position: center;
+  background-size: cover;
+  border-radius: .5rem;
+  border-color: paleturquoise;
+  border-style: solid;
+  border-width: .15em;
+  color: peru;
+  height: 25vh;
+}
+
+.event-card{
+  border: .5rem black solid .15em;
 }
 
 </style>
---
+++ The_Tower.client/src/services/EventsService.js
@@ -0,0 +1,12 @@
+import { AppState } from "../AppState"
+import { Event } from "../models/Event"
+import { api } from "./AxiosService"
+
+class EventsService{
+    async getEvents(){
+        const res = await api.get('api/events')
+        AppState.events = res.data.map(event => new Event(event))
+    }
+}
+
+export const eventsService = new EventsService()
