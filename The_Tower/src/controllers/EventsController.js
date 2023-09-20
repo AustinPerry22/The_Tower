0e0ced68157e6c26fcf0a3ec4a0c29270ec891d1
Austin <austinjperry22@gmail.com>
2023-09-20 10:31:37 -0600

    get events and post event works

--- The_Tower/src/controllers/EventsController.js
+++ The_Tower/src/controllers/EventsController.js
@@ -6,8 +6,8 @@ export class EventsController extends BaseController {
     constructor() {
         super('api/events')
         this.router
-            // .get('', this.getEvents)
-            // .get('/:eventId', this.getEventById)
+            .get('', this.getEvents)
+            .get('/:eventId', this.getEventById)
             // .get('/:eventId/comments', this.getCommentsInEvent)
             // .get('/:eventId/attendees', this.getAttendees)
             .use(Auth0Provider.getAuthorizedUserInfo)
@@ -15,6 +15,23 @@ export class EventsController extends BaseController {
         // .delete('/eventId', this.cancelEvent)
     }
 
+    async getEvents(req, res, next) {
+        try {
+            const events = await eventsService.getEvents()
+            res.send(events)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getEventById(req, res, next) {
+        try {
+            const event = await eventsService.getEventsById(req.params.eventId)
+            res.send(event)
+        } catch (error) {
+            next(error)
+        }
+    }
     async createEvent(req, res, next) {
         try {
             let eventBody = req.body
@@ -25,4 +42,5 @@ export class EventsController extends BaseController {
             next(error)
         }
     }
+
 }
--- The_Tower/src/services/EventsService.js
+++ The_Tower/src/services/EventsService.js
@@ -1,6 +1,14 @@
 import { dbContext } from "../db/DbContext.js"
 
 class EventsService {
+    async getEvents() {
+        const events = await dbContext.Events.find().populate('creator ticketCount')
+        return events
+    }
+    async getEventsById(eventId) {
+        const event = (await dbContext.Events.findById(eventId)).populate('creator ticketCount')
+        return event
+    }
     async createEvent(eventBody) {
         const event = await dbContext.Events.create(eventBody)
         await event.populate('creator ticketCount')
