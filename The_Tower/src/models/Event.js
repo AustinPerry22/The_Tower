10288fc809ef7298f83dca862eae44b5f96947d1
Austin <austinjperry22@gmail.com>
2023-09-22 10:26:51 -0600

    fixed max name

--- The_Tower/src/models/Event.js
+++ The_Tower/src/models/Event.js
@@ -3,7 +3,7 @@ import { Schema } from "mongoose";
 export const EventsSchema = new Schema(
     {
         creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
-        name: { type: String, required: true, maxLength: 25 },
+        name: { type: String, required: true, maxLength: 50 },
         description: { type: String, required: true, maxLength: 1000 },
         coverImg: { type: String, maxLength: 5000, default: 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60' },
         location: { type: String, maxLength: 100, default: 'online' },
